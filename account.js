4d1fbf1e2c247a37f9cf25ba2c207a06fb4d4846
BS/MFI <blackswan.mfi.0212@gmail.com>
2025-11-25 11:24:51 +0530

    Update account.js

--- account.js
+++ account.js
@@ -1,4 +1,4 @@
-// document.addEventListener('contextmenu', e => e.preventDefault());
+document.addEventListener('contextmenu', e => e.preventDefault());
 for(let i=0;i<15;i++){
     const b=document.createElement("div");b.className="bulb";
     const dur=6+Math.random()*5;
@@ -312,7 +312,7 @@ function displayLoanDetails(loan, index) {
             </div>
             <div class="details">
                 <div class="leftflow">
-                    <svg style='filter: invert(1);' xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24" fill="#000000"><g fill="none" stroke="#000000" stroke-width="1.5"><path stroke-linecap="round" d="M7 4V2.5M17 4V2.5"/><circle cx="16.5" cy="16.5" r="1.5"/><path stroke-linecap="round" d="M21.5 9H10.75M2 9h3.875M14 22h-4c-3.771 0-5.657 0-6.828-1.172C2 19.657 2 17.771 2 14v-2c0-3.771 0-5.657 1.172-6.828C4.343 4 6.229 4 10 4h4c3.771 0 5.657 0 6.828 1.172C22 6.343 22 8.229 22 12v2c0 3.771 0 5.657-1.172 6.828c-.653.654-1.528.943-2.828 1.07"/></g></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24" fill="#ffffffff"><g fill="none" stroke="#ffffffff" stroke-width="1.5"><path stroke-linecap="round" d="M7 4V2.5M17 4V2.5"/><circle cx="16.5" cy="16.5" r="1.5"/><path stroke-linecap="round" d="M21.5 9H10.75M2 9h3.875M14 22h-4c-3.771 0-5.657 0-6.828-1.172C2 19.657 2 17.771 2 14v-2c0-3.771 0-5.657 1.172-6.828C4.343 4 6.229 4 10 4h4c3.771 0 5.657 0 6.828 1.172C22 6.343 22 8.229 22 12v2c0 3.771 0 5.657-1.172 6.828c-.653.654-1.528.943-2.828 1.07"/></g></svg>
                     <h3 style="margin:25px 0;font-weight:600;font-size:15px;">Taken Date</h3>
                 </div>
                 <p>${loan.planDate}</p>
