80b50bf93d5bc1f403ca46ae2ff86e6f5c877106
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-01-01 22:18:35 +0530

    Update changable.js

--- changable.js
+++ changable.js
@@ -13,8 +13,9 @@ function getDailyInterestRate() {
   for (let i = 0; i < today.length; i++) {
     hash = ((hash << 5) + hash + today.charCodeAt(i)) | 0;
   }
-  const rate = 28 + (Math.abs(hash) % 5);
+  const rate = 23 + (Math.abs(hash) % 4);
   return rate;
 }
+
 
 
