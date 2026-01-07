70d291f311ec531a2b03cb164479eaa9c3755bcd
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-01-07 10:28:00 +0530

    Update account.js

--- account.js
+++ account.js
@@ -383,7 +383,7 @@ function displayLoanDetails(loan, index) {
 
     document.getElementById("loanDetails").innerHTML = `
         <div class="loan-entry">
-            <div class="details">
+            <div class="details"  style="transform: none;">
                 <div class="leftflow">
                     <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24"><g fill="none"><circle cx="11.5" cy="5.5" r="2.5" stroke="#ffffff" stroke-width="1.5"/><path stroke="#ffffff" stroke-linecap="round" stroke-width="1.5" d="M9 16.5s-.426 1.62-1 2.5c-.603.925-2 2-2 2"/><path fill="#ffffff" d="m10.21 11.901l.746.075l-.746-.075Zm-.069.686l-.746-.075l.746.075Zm8.436-2.734l-.48-.576l.48.576Zm.903.223a.75.75 0 0 0-.96-1.152l.96 1.152Zm-8.276 4.677l-.397.636l.397-.636Zm.333.208l.398-.636l-.398.636Zm2.72 4.432l.747-.067l-.747.067Zm-.604 1.674a.75.75 0 1 0 1.494-.134l-1.494.134ZM13 10.5l-.386.643a.75.75 0 0 0 .05.028L13 10.5Zm-3.536 1.326l-.069.686l1.492.15l.07-.686l-1.493-.15Zm9.593-1.397l.423-.353l-.96-1.152l-.424.353l.96 1.152Zm-8.25 4.96l.333.208l.795-1.272l-.333-.208l-.795 1.272Zm2.703 4.07l.143 1.608l1.494-.134l-.143-1.607l-1.494.133Zm-.845-8.288a5.878 5.878 0 0 0 6.392-.742l-.96-1.152a4.378 4.378 0 0 1-4.762.552l-.67 1.342Zm-1.525 4.426a5.086 5.086 0 0 1 2.37 3.862l1.494-.133a6.586 6.586 0 0 0-3.07-5.001l-.794 1.272Zm-1.745-3.085a3.036 3.036 0 0 0 1.412 2.877l.795-1.272a1.536 1.536 0 0 1-.715-1.455l-1.492-.15Zm1.561-.536a1.098 1.098 0 0 1 1.658-.833l.772-1.286c-1.634-.98-3.733.073-3.922 1.97l1.492.149Z"/><path stroke="#ffffff" stroke-linecap="round" stroke-width="1.5" d="M19 14V7m0 14v-3"/></g></svg>    
                     <h3 style="margin:25px 0;font-weight:600;font-size:15px;">Purpose</h3>
@@ -458,6 +458,11 @@ function displayLoanDetails(loan, index) {
                     ${formatMoney(totalPayable)}
                 </p>
             </div>
+            <a target="_blank" href="https://forms.gle/RzTJ8W9bwmm8DVj2A"><button style="background-color: rgb(160 0 0);
+    width: 100%;
+    padding: 10px;
+    margin-top: 30px;
+    font-size: 14px;" class="add-link-btn">I have an issue with my account.!</button></a>
         </div>
     `;
 }
@@ -581,7 +586,7 @@ function showDatePopup(idx) {
         <p style='color: #e2b325;font-weight: 600;    font-size: 20px;'><strong>Status:</strong> <span style="color:${daysLeft<=2?'#F44336':daysLeft<=6?'#FFCA28':'#4CAF50'}">
             ${daysLeft > 0 ? daysLeft + ' days left' : 'Overdue'}
         </span></p>
-        <button style="margin-top:15px;width:100%;padding:10px;background:#004fff;border-radius:20px;margin-bottom:-5px" onclick="goToList(${idx})">View Full</button>
+        <button style="margin-top:15px;width:100%;padding:8px;background:#004fff;border-radius:20px;margin-bottom:-5px" onclick="goToList(${idx})">View Full</button>
     `;
     document.getElementById('datePopup').style.display = 'block';
 }
@@ -744,3 +749,13 @@ const coinSection = document.getElementById('coinSection');
             updateGlow();
         });
         updateGlow();
+
+  window.addEventListener('load', () => {
+    document.querySelector('.modal').classList.add('active');
+  });
+
+  function closeModal() {
+    const modal = document.querySelector('.modal');
+    modal.classList.remove('active');
+    setTimeout(() => modal.style.display = 'none', 1000); // hide after animation
+  }
