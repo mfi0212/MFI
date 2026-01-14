a1125b5f63ffcb17a4edc21573956d2f93e9c076
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-01-14 09:54:42 +0530

    Update account.js

--- account.js
+++ account.js
@@ -140,9 +140,10 @@ let filteredLoans = [];
 const usersDB = {
     "Mahesh888*": {
         name: "Mahesh Muthinti",
-        coins: 585,
+        coins: 0,
         loans: [
             { planDate: "11-01-2026", endDate: "10-02-2026", interest: 690, takenAmount: 3000, takenFrom: "Golden", fineRate: 50 },
+            { planDate: "14-01-2026", endDate: "13-02-2026", interest: 2551, takenAmount: 12000, takenFrom: "Golden", fineRate: 50 },
         ],
         links: [],
         emote: "https://media.tenor.com/pT6HQx4wIogAAAAj/twitch-rpx-syria.gif"
