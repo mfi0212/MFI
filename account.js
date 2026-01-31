fb5285165cf94b99dda5afe95bdb0aa64de1b640
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-01-31 23:34:05 +0530

    Update account.js

--- account.js
+++ account.js
@@ -149,6 +149,19 @@ const usersDB = {
         links: [],
         emote: "https://media.tenor.com/pT6HQx4wIogAAAAj/twitch-rpx-syria.gif"
     },
+    "0212": {
+        name: "Tony",
+        coins: 0,
+        loans: [
+            { planDate: "11-01-2026", endDate: "10-02-2026", interest: 690, takenAmount: 3000, takenFrom: "Golden", fineRate: 50 },
+            { planDate: "14-01-2026", endDate: "13-02-2026", interest: 2661, takenAmount: 12000, takenFrom: "Golden", fineRate: 50 },
+            { planDate: "25-01-2026", endDate: "24-02-2026", interest: 1085, takenAmount: 3500, takenFrom: "Golden", fineRate: 50 },
+            { planDate: "25-01-2026", endDate: "24-02-2026", interest: 1085, takenAmount: 3500, takenFrom: "Golden", fineRate: 50 },
+            { planDate: "25-01-2026", endDate: "24-02-2026", interest: 1085, takenAmount: 3500, takenFrom: "Golden", fineRate: 50 },
+        ],
+        links: [],
+        emote: "https://media.tenor.com/pT6HQx4wIogAAAAj/twitch-rpx-syria.gif"
+    },
 };
 
 function loadUserData() {
