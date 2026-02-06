f1cfdde65214590b8394095b85f50ea0b0b92eb8
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-02-06 22:31:37 +0530

    Update account.js

--- account.js
+++ account.js
@@ -457,8 +457,10 @@ function displayLoanDetails(loan, index) {
             <div class="totaldetails">
                 <svg style='width: 30px;height: 30px;margin-top: 25px;opacity: 80%;' xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24" fill="#ffffff"><g fill="none" stroke="#ffffff" stroke-width="1.5"><path d="M17.414 10.414C18 9.828 18 8.886 18 7c0-1.886 0-2.828-.586-3.414m0 6.828C16.828 11 15.886 11 14 11h-4c-1.886 0-2.828 0-3.414-.586m10.828 0Zm0-6.828C16.828 3 15.886 3 14 3h-4c-1.886 0-2.828 0-3.414.586m10.828 0Zm-10.828 0C6 4.172 6 5.114 6 7c0 1.886 0 2.828.586 3.414m0-6.828Zm0 6.828ZM13 7a1 1 0 1 1-2 0a1 1 0 0 1 2 0Z"/><path stroke-linecap="round" d="M18 6a3 3 0 0 1-3-3m3 5a3 3 0 0 0-3 3M6 6a3 3 0 0 0 3-3M6 8a3 3 0 0 1 3 3M4 21.388h2.26c1.01 0 2.033.106 3.016.308a14.85 14.85 0 0 0 5.33.118m-.93-3.297c.12-.014.235-.03.345-.047c.911-.145 1.676-.633 2.376-1.162l1.808-1.365a1.887 1.887 0 0 1 2.22 0c.573.433.749 1.146.386 1.728c-.423.678-1.019 1.545-1.591 2.075m-5.544-1.229a8.176 8.176 0 0 1-.11.012m.11-.012a.998.998 0 0 0 .427-.24a1.492 1.492 0 0 0 .126-2.134a1.9 1.9 0 0 0-.45-.367c-2.797-1.669-7.15-.398-9.779 1.467m9.676 1.274a.524.524 0 0 1-.11.012m0 0a9.274 9.274 0 0 1-1.814.004"/></g></svg>
                 <h3 style="margin: 10px 0;font-weight: 600;font-size: 15px;color: #ffffff91;">Total amount to return</h3>
-                <p style="font-size:60px;color:${overdueFine > 0 ? '#ff4000ff' : '#00b900ff'};font-weight:bold;
-font-family: 'Gantari', sans-serif;">
+                <p style="font-size:60px;color:${overdueFine > 0 ? '#ff4000ff' : '#00b900ff'};
+    font-weight: 600;
+    font-family: 'Anton', sans-serif;
+    letter-spacing: 4.5px;">
                     ${formatMoney(totalPayable)}
                 </p>
                  <a target="_blank"  href="https://forms.gle/RzTJ8W9bwmm8DVj2A"><button style="background-color: rgb(255 0 0 / 69%);
@@ -597,16 +599,21 @@ function showDatePopup(idx) {
         <p style='color: #e2b325;font-weight: 600;'><strong>Taken Amount:</strong> ${formatMoney(loan.takenAmount)}</p>
         <p style='color: #e2b325;font-weight: 600;'><strong>Purpose:</strong> ${loan.purpose || 'Not set'}</p>
         <p style='color: #e2b325;font-weight: 600;'><strong>Return date :</strong> ${cleanEnd}</p>
-        <hr style='margin: 0px;'>
+        <hr style='margin: 5px;'>
         <p style='color: #e2b325;font-weight: 600;    font-size: 20px;'><strong>Status:</strong> <span style="color:${daysLeft<=2?'#F44336':daysLeft<=6?'#FFCA28':'#4CAF50'}">
             ${daysLeft > 0 ? daysLeft + ' days left' : 'Overdue'}
         </span></p>
         <button style="width: 100%;
     padding: 8px;
-    background: #004fff;
+    background: #0026ff;
     border-radius: 20px;
     margin-bottom: -5px;
-    margin-top: 5px;" onclick="goToList(${idx})">View Full</button>
+    margin-top: 5px;" onclick="goToList(${idx})">View Full Details</button>
+    <button onclick="closeDatePopup()" style="width: 100%;
+    padding: 8px;
+    background: #ff0000;
+    border-radius: 20px;
+    margin-bottom:0px;margin-top: 20px;background-color: red;" onclick="goToList(${idx})">Close Now</button>
     `;
     document.getElementById('datePopup').style.display = 'block';
 }
