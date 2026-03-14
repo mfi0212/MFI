fe211b6f2222e76dd2a9708041767fbc87e32155
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-03-14 17:19:05 +0530

    Update account.js

--- account.js
+++ account.js
@@ -151,7 +151,7 @@ const usersDB = {
         emote: "https://media.tenor.com/pT6HQx4wIogAAAAj/twitch-rpx-syria.gif"
     },
     "0212": {
-        name: "Tony Mantana",
+        name: "Tony Mantana ⚡",
         coins: 4000,
         loans: [
              { planDate: "09-02-2026", endDate: "08-03-2026", interest: 1340, takenAmount: 12460, takenFrom: "Lendlink", fineRate: 50 },
