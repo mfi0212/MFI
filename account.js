b182616c5e7e047d7000244855451dc2dfbd5663
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-05-25 22:02:40 +0530

    Update account.js

--- account.js
+++ account.js
@@ -24,7 +24,7 @@ const usersDB = {
         defaultEmote: "https://media.tenor.com/cxAQToMOeykAAAAj/twitch-rpx-syria.gif"
     },
     "0212": {
-        name: "Montana",
+        name: "Tony Montana",
         coins: 4000,
         loans: [
              { planDate: "09-02-2026", endDate: "01-05-2026", interest: 1340, takenAmount: 12460, takenFrom: "Lendlink", fineRate: 50 },
