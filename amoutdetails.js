28003e4bd1f5586b933ae1fe2d6a7ab0ddaaf210
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-06-08 17:37:38 +0530

    Add files via upload

---
+++ amoutdetails.js
@@ -0,0 +1,24 @@
+const usersDB = {
+   "Mahesh888*": {
+        name: "Mahesh Muthinti",
+        coins: 0,
+        loans: [
+            { planDate: "11-05-2026", endDate: "11-06-2026", interest: 3560, takenAmount: 15000, takenFrom: "Golden", fineRate: 130 },
+            { planDate: "25-05-2026", endDate: "24-06-2026", interest: 500, takenAmount: 2000, takenFrom: "Golden", fineRate: 130 },
+        ],
+        links: [],
+       emote: "https://media.tenor.com/cxAQToMOeykAAAAj/twitch-rpx-syria.gif",
+        defaultEmote: "https://media.tenor.com/cxAQToMOeykAAAAj/twitch-rpx-syria.gif"
+    },
+    "0212": {
+        name: "Tony Montana",
+        coins: 0,
+        loans: [
+             { planDate: "09-02-2026", endDate: "10-06-2026", interest: 1340, takenAmount: 10860, takenFrom: "Lendlink", fineRate: 50 },
+             { planDate: "09-02-2026", endDate: "30-08-2026", interest: 1340, takenAmount: 6460, takenFrom: "Lendlink", fineRate: 50 },
+             { planDate: "09-02-2026", endDate: "01-08-2026", interest: 1340, takenAmount: 9460, takenFrom: "Lendlink", fineRate: 50 },            ],
+        links: [],
+        emote: "https://media.tenor.com/pT6HQx4wIogAAAAj/twitch-rpx-syria.gif",
+        defaultEmote: "https://files.donationalerts.com/uploads/images/2/tb_5000.gif"
+    },
+};
