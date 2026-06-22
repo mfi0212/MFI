4eb24cb68686fc1b32b90f7a33c3cb6381f4dd9c
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-06-22 18:21:41 +0530

    Update amoutdetails.js

--- amoutdetails.js
+++ amoutdetails.js
@@ -7,19 +7,18 @@ const usersDB = {
             { planDate: "11-05-2026", endDate: "10-07-2026", interest: 4640, takenAmount: 18560, takenFrom: "Golden", fineRate: 130 },
             { planDate: "10-06-2026", endDate: "10-07-2026", interest: 232, takenAmount: 1138, takenFrom: "Delay & Mansion Fee", fineRate: 130 },
         ],
-        links: [],
-       emote: "https://media.tenor.com/cxAQToMOeykAAAAj/twitch-rpx-syria.gif",
-        defaultEmote: "https://media.tenor.com/cxAQToMOeykAAAAj/twitch-rpx-syria.gif"
+        fragment: "",
+    defaultEmote: "https://files.donationalerts.com/uploads/images/2/tb_5000.gif"
     },
     "0212": {
         name: "Tony Montana",
         coins: 0,
         loans: [
-             { planDate: "09-02-2026", endDate: "10-06-2026", interest: 1340, takenAmount: 10860, takenFrom: "Lendlink", fineRate: 50 },
-             { planDate: "09-02-2026", endDate: "30-08-2026", interest: 1340, takenAmount: 6460, takenFrom: "Lendlink", fineRate: 50 },
-             { planDate: "09-02-2026", endDate: "01-08-2026", interest: 1340, takenAmount: 9460, takenFrom: "Lendlink", fineRate: 50 },            ],
-        links: [],
-        emote: "https://media.tenor.com/pT6HQx4wIogAAAAj/twitch-rpx-syria.gif",
-        defaultEmote: "https://files.donationalerts.com/uploads/images/2/tb_5000.gif"
-    },
+            { planDate: "09-02-2026", endDate: "03-07-2026", interest: 1340, takenAmount: 10860, takenFrom: "Lendlink", fineRate: 50 },
+            { planDate: "09-02-2026", endDate: "30-08-2026", interest: 1340, takenAmount: 6460, takenFrom: "Lendlink", fineRate: 50 },
+            { planDate: "09-02-2026", endDate: "01-08-2026", interest: 1340, takenAmount: 9460, takenFrom: "Lendlink", fineRate: 50 }
+        ],
+        fragment: "https://raw.githubusercontent.com/goforbg/telegram-emoji-gifs/refs/heads/master/dead-skull.gif",
+    defaultEmote: "https://files.donationalerts.com/uploads/images/2/tb_5000.gif"
+    }
 };
