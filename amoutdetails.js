610450abde8a8f9ce40e0e8b2294cbbecf9f1e6f
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-06-27 11:20:14 +0530

    Update amoutdetails.js

--- amoutdetails.js
+++ amoutdetails.js
@@ -11,7 +11,7 @@ const usersDB = {
     },
     "0212": {
         name: "Tony Montana",
-        coins: 0,
+        coins: 2200,
         loans: [
             { planDate: "02-05-2026", endDate: "26-07-2026", interest: 2500, takenAmount: 10000, takenFrom: "Lendlink", fineRate: 0 }
         ],
