c1a6cf28d99d2e1a9961220d91658cdff79a7795
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-06-27 18:38:14 +0530

    Update amoutdetails.js

--- amoutdetails.js
+++ amoutdetails.js
@@ -6,7 +6,7 @@ const usersDB = {
             { planDate: "11-05-2026", endDate: "10-07-2026", interest: 4640, takenAmount: 18560, takenFrom: "Golden", fineRate: 130 },
             { planDate: "10-06-2026", endDate: "10-07-2026", interest: 232, takenAmount: 1138, takenFrom: "Delay & Mansion Fee", fineRate: 130 },
         ],
-        fragment: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/refs/heads/main/Symbols/New%20Button.webp",
+        fragment: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/refs/heads/main/Symbols/Top%20Arrow.webp",
     defaultEmote: "service-icons/premium_mini_logo.gif"
     },
     "0212": {
