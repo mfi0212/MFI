e3e068499ca953de7721db96ef51ae27c63b0e0a
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-06-30 08:48:51 +0530

    Update amoutdetails.js

--- amoutdetails.js
+++ amoutdetails.js
@@ -3,7 +3,7 @@ const usersDB = {
         name: "Mahesh Muthinti",
         coins: 0,
         loans: [
-            { planDate: "11-05-2026", endDate: "10-07-2026", interest: 4640, takenAmount: 18560, takenFrom: "Golden", fineRate: 130 },
+            { planDate: "11-05-2026", endDate: "10-08-2026", interest: 5800, takenAmount: 23200, takenFrom: "Golden", fineRate: 130 },
         ],
         fragment: "",
     defaultEmote: "service-icons/premium_mini_logo.gif"
@@ -14,7 +14,7 @@ const usersDB = {
         loans: [
             { planDate: "02-05-2026", endDate: "26-07-2026", interest: 1250, takenAmount: 5000, takenFrom: "Lendlink", fineRate: 0 }
         ],
-       fragment: "",
+       fragment: "https://data.chpic.su/stickers/t/ToyBearGift_by_EmojiRu_Bot/ToyBearGift_by_EmojiRu_Bot_011.webp",
     defaultEmote: ""
     }
 };
