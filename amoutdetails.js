7c10de733a9ec86f82ef82ffc682ed8f890e596d
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-07-02 10:31:30 +0530

    Update amoutdetails.js

--- amoutdetails.js
+++ amoutdetails.js
@@ -9,12 +9,14 @@ const usersDB = {
     defaultEmote: "service-icons/premium_mini_logo.gif"
     },
     "0212": {
-        name: "Tony Montana",
-        coins: 0,
+        name: "American Patriots",
+        coins: 1776,
         loans: [
-            { planDate: "02-05-2026", endDate: "26-07-2026", interest: 1250, takenAmount: 5000, takenFrom: "Lendlink", fineRate: 0 }
+            { planDate: "02-05-2026", endDate: "26-07-2026", interest: 2900, takenAmount: 15000, takenFrom: "Lendlink", fineRate: 0 },
+            { planDate: "02-05-2026", endDate: "26-07-2026", interest: 2300, takenAmount: 10000, takenFrom: "Lendlink", fineRate: 0 },
+            { planDate: "02-05-2026", endDate: "26-07-2026", interest: 1250, takenAmount: 5000, takenFrom: "Lendlink", fineRate: 0 },
         ],
-       fragment: "https://data.chpic.su/stickers/t/ToyBearGift_by_EmojiRu_Bot/ToyBearGift_by_EmojiRu_Bot_011.webp",
-    defaultEmote: ""
+       fragment: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/refs/heads/main/Flags/Flag%20United%20States.webp",
+    defaultEmote: "https://png.pngtree.com/png-vector/20230918/ourmid/pngtree-soldier-of-the-american-revolution-holding-the-flag-with-stars-png-image_9760261.png"
     }
 };
