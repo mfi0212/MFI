c18ad4d280cf9d0987ae617586a7210f249f3f6b
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-07-04 00:40:54 +0530

    Update amoutdetails.js

--- amoutdetails.js
+++ amoutdetails.js
@@ -1,13 +1,4 @@
 const usersDB = {
-   "Mahesh888*": {
-        name: "Mahesh Muthinti",
-        coins: 0,
-        loans: [
-            { planDate: "11-05-2026", endDate: "10-08-2026", interest: 5800, takenAmount: 23200, takenFrom: "Golden", fineRate: 130 },
-        ],
-        fragment: "",
-    defaultEmote: "service-icons/premium_mini_logo.gif"
-    },
     "0212": {
         name: "Tony Montana",
         coins: 0,
