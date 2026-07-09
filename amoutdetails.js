5231752df9f155fd1ff0932fb9055c8c155dff51
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-07-09 19:46:50 +0530

    Update amoutdetails.js

--- amoutdetails.js
+++ amoutdetails.js
@@ -1,12 +1,12 @@
 const usersDB = {
     "0212": {
-        name: "Tony Momtana",
-        coins: 0,
+        name: "Pavel Durov",
+        coins: 2000,
         loans: [
-            { planDate: "02-05-2026", endDate: "26-07-2026", interest: 2300, takenAmount: 10000, takenFrom: "Lendlink", fineRate: 0 },
+            { planDate: "02-05-2026", endDate: "26-07-2026", interest: 3200, takenAmount: 15000, takenFrom: "Lendlink", fineRate: 0 },
             { planDate: "02-05-2026", endDate: "26-07-2026", interest: 1250, takenAmount: 5000, takenFrom: "Lendlink", fineRate: 0 },
         ],
-       fragment: "",
-    defaultEmote: "service-icons/premium_mini_logo.gif"
+       fragment: "https://data.chpic.su/stickers/e/Easteregggifts_by_EmojiRu_Bot/Easteregggifts_by_EmojiRu_Bot_034.webp",
+    defaultEmote: "https://static4.tgstat.ru/channels/_0/1c/1c67df9e0a5cfefa030b853983324004.jpg"
     }
 };
