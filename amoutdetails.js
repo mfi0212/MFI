d71bb9480aafa848534c1e9cd830c1c35e465c8d
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-07-14 00:50:38 +0530

    Update amoutdetails.js

--- amoutdetails.js
+++ amoutdetails.js
@@ -1,12 +1,12 @@
 const usersDB = {
     "0212": {
-        name: "Tony Momtana",
-        coins: 0,
+        name: "Venkats Siva Yerra",
+        coins: 2000,
         loans: [
             { planDate: "02-05-2026", endDate: "26-07-2026", interest: 2300, takenAmount: 10000, takenFrom: "Lendlink", fineRate: 0 },
             { planDate: "02-05-2026", endDate: "26-07-2026", interest: 1250, takenAmount: 5000, takenFrom: "Lendlink", fineRate: 0 },
         ],
-       fragment: "",
-    defaultEmote: "service-icons/premium_mini_logo.gif"
+       fragment: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/refs/heads/main/Animals%20and%20Nature/Fire.webp",
+    defaultEmote: "https://scontent.cdninstagram.com/v/t51.82787-19/742437325_18059034488746087_5971811685419518234_n.jpg?_nc_cat=105&ccb=7-5&_nc_sid=bf7eb4&efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLnd3dy4xMDgwLkMzIn0%3D&_nc_ohc=lhsjJCDjh74Q7kNvwE5NnIe&_nc_oc=AdrxClK1TG7sqMD3DYCNdgy2gkIYCZThF7b4YxtV57V3uO6p81hftnoyfFvQIZJesgk&_nc_zt=24&_nc_ht=scontent.cdninstagram.com&_nc_gid=bmnuW1CXKEJygLLEqIkYdQ&_nc_ss=7baaf&oh=00_AQALWWSJnUpNhUD8OVXCDKpCcInggzYAPQfZJzRBTRcehw&oe=6A5B02CC"
     }
 };
