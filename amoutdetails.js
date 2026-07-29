64ac59d6828331e069dfc1c01942c54b686a263f
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-07-29 13:57:42 +0530

    Update amoutdetails.js

--- amoutdetails.js
+++ amoutdetails.js
@@ -1,10 +1,10 @@
 const usersDB = {
     "9919": {
         name: "Charlie",
-        coins: 2000,
+        coins: 205,
         loans: [
-            { planDate: "02-05-2026", endDate: "26-09-2026", interest: 2300, takenAmount: 10000, takenFrom: "Lendlink", fineRate: 0 },
-            { planDate: "02-05-2026", endDate: "26-09-2026", interest: 1250, takenAmount: 5000, takenFrom: "Lendlink", fineRate: 0 },
+            { planDate: "02-05-2026", endDate: "26-09-2026", interest: 2300, takenAmount: 2907, takenFrom: "Lendlink", fineRate: 0 },
+            { planDate: "02-05-2026", endDate: "26-09-2026", interest: 1250, takenAmount: 2026, takenFrom: "Lendlink", fineRate: 0 },
         ],
        fragment: "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_b431fc1e14d642a781c8116343c15967/default/dark/3.0#e=0",
     defaultEmote:"https://media.tenor.com/cxAQToMOeykAAAAj/twitch-rpx-syria.gif"
