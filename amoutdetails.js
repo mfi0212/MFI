21b997ed069090159a022ae603ea8b20fb75fcce
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-08-06 15:13:35 +0530

    Update amoutdetails.js

--- amoutdetails.js
+++ amoutdetails.js
@@ -1,14 +1,14 @@
 const usersDB = {
     "9919": {
-        name: "Pskry",
-        coins: 3205,
+        name: "Charlie",
+        coins: 205,
         loans: [
             { planDate: "02-05-2026", endDate: "26-09-2026", interest: 2300, takenAmount: 21907, takenFrom: "Lendlink", fineRate: 0 },
             { planDate: "02-05-2026", endDate: "16-09-2026", interest: 1250, takenAmount: 12000, takenFrom: "Lendlink", fineRate: 0 },
             { planDate: "02-05-2026", endDate: "6-09-2026", interest: 1250, takenAmount: 20026, takenFrom: "Lendlink", fineRate: 0 },
         ],
-        fragment: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/refs/heads/main/Flags/Flag%20India.webp",
-        defaultEmote: "https://scontent.cdninstagram.com/v/t51.82787-19/729808268_18106195441988721_2475922297071123598_n.jpg?_nc_cat=102&ccb=7-5&_nc_sid=bf7eb4&efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLnd3dy4xMDgwLkMzIn0%3D&_nc_ohc=MT7bDHgKvaAQ7kNvwHgY194&_nc_oc=AdqD7H6FTnpgRCQ_AKhYFDK9Bv2U7w8hA_cHbLxvJTpVhbiNyL72tWJO7xg2P1o28lw&_nc_zt=24&_nc_ht=scontent.cdninstagram.com&_nc_gid=020JzCbJWuBei1EYUOI0gg&_nc_ss=7baaf&oh=00_AQHeFh5qfJokqlp2Gdx86LtRXVoGl0hlCaHGkM2RspHqXw&oe=6A7A10E4",
+        fragment: "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_b431fc1e14d642a781c8116343c15967/default/dark/3.0#e=0",
+        defaultEmote: "https://media.tenor.com/cxAQToMOeykAAAAj/twitch-rpx-syria.gif",
         showCustomContent: "yes",
         customContent: {
             type: "image",
@@ -16,7 +16,7 @@ const usersDB = {
             url: "https://mfi0212.github.io/swan/offer/solution"
         },
         showSpecialNotice: "yes",
-        specialNoticeText: "Dear <strong>Pskry</strong>, Welcome to OFFER CLAIMER TLL AUGUST 15TH.!"
+        specialNoticeText: "Dear <strong>Chmbad arlie</strong>, multiple Lendlink loans are active. Please clear them before the earliest due date to avoid extra charges."
     },
 };
 
