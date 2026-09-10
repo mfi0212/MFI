9c6fb32fe96a715216f602e0ebdaeb47bb8578f5
BS/MFI <blackswan.mfi.0212@gmail.com>
2026-09-11 01:04:47 +0530

    Update new-service-amount-details.js

--- new-service-amount-details.js
+++ new-service-amount-details.js
@@ -2,9 +2,9 @@ const users = {
   "Mahesh Muthinti": {
     password: "Mahesh888*",
     displayName: "Mahesh Muthinti",
-    premiumType: "Premium+",
+    premiumType: "normal",
     fixedInterest: 30,
-    customui: "yes",
+    customui: "no",
     showCustomContent: "no",
     customContent: {
       type: "image",
@@ -15,13 +15,7 @@ const users = {
     specialNoticeText: "",
 
     loans: [
-      { amount: 29418,interest: 7710, borrowed: "11-05-2026", return: "12-09-2026", overdueFee: 0 },
-      { amount: 15000,interest: 4500, borrowed: "15-08-2026", return: "15-09-2026", overdueFee: 0 },
-      { amount: 3475, interest: 1407, borrowed: "21-07-2026", return: "20-09-2026", overdueFee: 0 },
-      { amount: 2990, interest: 1170, borrowed: "24-07-2026", return: "23-09-2026", overdueFee: 0 },
-      { amount: 3940, interest: 1280,  borrowed: "28-07-2026", return: "28-09-2026", overdueFee: 25 },
-      { amount: 1210,  interest: 650,  borrowed: "15-08-2026", return: "29-09-2026", overdueFee: 0 },
-      { amount: 1000,interest: 650, borrowed: "31-08-2026", return: "30-10-2026", overdueFee: 0 },
+     
     ],
     access: {
       "Delay It": true,
